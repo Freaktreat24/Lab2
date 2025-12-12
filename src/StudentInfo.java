55d0b42134e3cfc28f37c137cb40b7c8fb1f8e56
Anmol5009 <aryaanmol29@gmail.com>
2025-12-12 13:03:51 +0530

    initial commit

---
+++ src/StudentInfo.java
@@ -0,0 +1,9 @@
+class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Anmol Arya");
+        System.out.println("Roll No: 241155009");
+        System.out.println("Section: 33");
+        System.out.println("Branch: CSE");
+    }
+}
+
