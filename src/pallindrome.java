eadecf66cb945547f1add630647a4ed57a3180c3
Anmol5009 <aryaanmol29@gmail.com>
2025-12-12 13:41:36 +0530

    fourth commit

---
+++ src/pallindrome.java
@@ -0,0 +1,20 @@
+class palindrome {
+    public static void main(String[] args) {
+        int num = 121;   // 👉 Mention the number here
+        int original = num;
+        int reversed = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num = num / 10;
+        }
+
+        if (original == reversed) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is NOT a Palindrome");
+        }
+    }
+}
+
