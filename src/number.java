7cf3f5c8efef0af37aa070f09066c39463768f07
Anmol5009 <aryaanmol29@gmail.com>
2025-12-19 12:41:39 +0530

    sixth commit

---
+++ src/number.java
@@ -0,0 +1,24 @@
+public class number {
+    public static void main(String[] args) {
+        int even = 0;
+        int odd = 0;
+
+        if (args.length != 10) {
+            System.out.println("Please enter exactly 10 integers as command line arguments.");
+            return;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(args[i]);
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Number of even numbers: " + even);
+        System.out.println("Number of odd numbers: " + odd);
+    }
+}
+
